f58af73bad57ed512d0fae9c348344a2722c5c77
Tikhova Mariya <tmg1999@yandex.ru>
2021-11-01 18:24:00 +0300

    создание таблицы и вставка продуктов осуществляется через ProductDatabase

--- src/main/java/ru/akirakozov/sd/refactoring/Main.java
+++ src/main/java/ru/akirakozov/sd/refactoring/Main.java
@@ -3,29 +3,18 @@ package ru.akirakozov.sd.refactoring;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import ru.akirakozov.sd.refactoring.database.ProductDatabase;
 import ru.akirakozov.sd.refactoring.servlet.AddProductServlet;
 import ru.akirakozov.sd.refactoring.servlet.GetProductsServlet;
 import ru.akirakozov.sd.refactoring.servlet.QueryServlet;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
 
 /**
  * @author akirakozov
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
-                    "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
-                    " NAME           TEXT    NOT NULL, " +
-                    " PRICE          INT     NOT NULL)";
-            Statement stmt = c.createStatement();
-
-            stmt.executeUpdate(sql);
-            stmt.close();
-        }
+        ProductDatabase database = new ProductDatabase("jdbc:sqlite:test.db");
+        database.createTableIfNotExist();
 
         Server server = new Server(8081);
 
